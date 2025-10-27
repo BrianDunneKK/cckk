d779a5bb93de0e671bfe5d1a3d684385852363a0
BrianDunneKK <Brian.Dunne.KK@gmail.com>
2025-10-27 23:04:26 +0000

    Create cckkImage class

--- cckkImage.py
+++ cckkImage.py
@@ -66,3 +66,90 @@ def imgPrint(imgAA):
         str += "\n"
     print(str)
 
+
+
+class cckkImage:
+    """Class representation of an image on a SenseHat"""
+    imgAA = None
+
+    def __init__(self, imgA = None):
+        self.imgAA = None
+        if (imgA is not None):
+            self.setFromArray(imgA)
+
+    def setFromArray(self, imgA, ncols):
+        self.imgAA = [imgA[i:i+ncols] for i in range(0, len(imgA), ncols)]
+        return self
+
+    def pixels(self):
+        imgA = []
+        for row in self.imgAA:
+            for pixel in row:
+                imgA.append(pixel)
+        return imgA
+
+    @property
+    def nrows(self):
+        """No. of rows in the image"""
+        return len(self.imgAA)
+
+    @property
+    def ncols(self):
+        """No. of columns in the image"""
+        return len(self.imgAA[0])
+
+    def roll(self, dx, dy):
+        result = []
+        for r in range(nrows):
+            new_r = (r - dy + self.nrows) % self.nrows
+            new_row = []
+            for c in range(ncols):
+                new_c = (c - dx + self.ncols) % self.ncols
+                new_row.append(self.imgAA[new_r][new_c])
+            result.append(new_row)
+        self.imgAA = result
+        return self
+
+    def imgMove(self, dx, dy, fill):
+        resultX = []
+        result = []
+        
+        if dx > 0 & dx <= self.ncols: # move right
+            for r in range(self.nrows):
+                new_row = [fill]*dx
+                for c in range(self.ncols-dx):
+                    new_row.append(self.imgAA[r][c])
+                resultX.append(new_row)
+        elif dx < 0 & dx >= -self.ncols: # move left
+            for r in range(self.nrows):
+                new_row = []
+                for c in range(-dx, self.ncols):
+                    new_row.append(self.imgAA[r][c])
+                new_row += [fill] * (-dx)
+                resultX.append(new_row)
+        else: # dx == 0 or invalid
+            resultX = [row[:] for row in self.imgAA]
+
+        if dy > 0 & dy <= self.nrows: # move down
+            for r in range(dy):
+                result.append([fill] * self.ncols)
+            for r in range(self.nrows - dy):
+                result.append(resultX[r])
+        elif dy < 0 & dy >= -self.nrows: # move up
+            for r in range(-dy, self.nrows):
+                result.append(resultX[r])
+            for r in range(-dy):
+                result.append([fill] * self.ncols)
+        else: # dy == 0 or invalid
+            result = [row[:] for row in resultX]
+
+        self.imgAA = result
+        return self
+
+    def imgPrint(self):
+        str = ""
+        for row in self.imgAA:
+            for pixel in row:
+                str += pixel + " "
+            str += "\n"
+        print(str)
