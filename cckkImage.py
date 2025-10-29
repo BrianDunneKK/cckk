1b02f822aaae477d8a28c7f8d50afdad62526403
BrianDunneKK <Brian.Dunne.KK@gmail.com>
2025-10-29 00:31:19 +0000

    Add method ot set image position

--- cckkImage.py
+++ cckkImage.py
@@ -1,11 +1,11 @@
 class cckkImage:
     """Class representation of an image on a SenseHat"""
-    imgAA = None
-    img_xpos = 0    # X-position of the image relative to the camera
-    img_ypos = 0    # Y-position of the image relative to the camera
-    camera_cols = 0 # Number of columns in the camera
-    camera_rows = 0 # Number of rows in the camera
-    camera_fill = None # Fill colour if the image does not fill the camera. Default: [0,0,0] (black)
+    _imgAA = None
+    _img_xpos = 0    # X-position of the image relative to the camera
+    _img_ypos = 0    # Y-position of the image relative to the camera
+    _camera_cols = 0 # Number of columns in the camera
+    _camera_rows = 0 # Number of rows in the camera
+    _camera_fill = None # Fill colour if the image does not fill the camera. Default: [0,0,0] (black)
 
     def __init__(self, imgA = None, img_cols = 8, camera_cols = 8, camera_rows = 8, camera_horiz = "C", camera_vert = "C", camera_fill = [0,0,0]):
         """Contructs a cckkImage object
@@ -25,55 +25,72 @@ class cckkImage:
         Raises:
         Exception: If invalid image specified
         """
-        self.camera_cols = camera_cols
-        self.camera_rows = camera_rows
-        self.camera_fill = camera_fill
+        self._camera_cols = camera_cols
+        self._camera_rows = camera_rows
+        self._camera_fill = camera_fill
 
         if (imgA is not None):
             self.setFromArray(imgA, img_cols, camera_horiz, camera_vert)
 
     def setFromArray(self, imgA, img_cols = 8, camera_horiz = "C", camera_vert = "C"):
-        self.imgAA = [imgA[i:i+img_cols] for i in range(0, len(imgA), img_cols)]
+        self._imgAA = [imgA[i:i+img_cols] for i in range(0, len(imgA), img_cols)]
         self.set_camera(camera_horiz, camera_vert)
         return self
 
     def set_camera(self, camera_horiz = "C", camera_vert = "C"):
         if camera_horiz.upper() == "L":
-            self.img_xpos = 0
+            self._img_xpos = 0
         elif camera_horiz.upper() == "R":
-            self.img_xpos = self.camera_cols - self.img_cols
+            self._img_xpos = self._camera_cols - self.img_cols
         else:
-            self.img_xpos = int((self.camera_cols - self.img_cols)/2)
+            self._img_xpos = int((self._camera_cols - self.img_cols)/2)
         
         if camera_vert.upper() == "T":
-            self.img_ypos = 0
+            self._img_ypos = 0
         elif camera_vert.upper() == "B":
-            self.img_ypos = self.camera_rows - self.img_rows
+            self._img_ypos = self._camera_rows - self.img_rows
         else:
-            self.img_ypos = int((self.camera_rows - self.img_rows)/2)
+            self._img_ypos = int((self._camera_rows - self.img_rows)/2)
 
         return self
 
     def pixels(self):
         """Image as seen through the camera"""
-        camera_imgA = [self.camera_fill] * (self.camera_cols * self.camera_rows)
-        for row in range(self.camera_rows):
-            for col in range(self.camera_cols):
-                col_img = col - self.img_xpos
-                row_img = row - self.img_ypos
+        camera_imgA = [self._camera_fill] * (self._camera_cols * self._camera_rows)
+        for row in range(self._camera_rows):
+            for col in range(self._camera_cols):
+                col_img = col - self._img_xpos
+                row_img = row - self._img_ypos
                 if (col_img >= 0 and col_img < self.img_cols and row_img >= 0 and row_img < self.img_rows):
-                    camera_imgA[row*self.camera_rows + col] = self.imgAA[row_img][col_img]
+                    camera_imgA[row*self._camera_rows + col] = self._imgAA[row_img][col_img]
         return camera_imgA
 
     @property
     def img_rows(self):
         """No. of rows in the image"""
-        return len(self.imgAA)
+        return len(self._imgAA)
 
     @property
     def img_cols(self):
         """No. of columns in the image"""
-        return len(self.imgAA[0])
+        return len(self._imgAA[0])
+
+    @property
+    def xpos(self):
+        return self._img_xpos
+
+    @xpos.setter
+    def xpos(self, value):
+        self._img_xpos = value
+
+    @property
+    def ypos(self):
+        return self._img_ypos
+
+    @xpos.setter
+    def ypos(self, value):
+        self._img_ypos = value
+
 
     def roll(self, dx, dy):
         result = []
@@ -82,54 +99,19 @@ class cckkImage:
             new_row = []
             for c in range(ncols):
                 new_c = (c - dx + self.img_cols) % self.img_cols
-                new_row.append(self.imgAA[new_r][new_c])
+                new_row.append(self._imgAA[new_r][new_c])
             result.append(new_row)
-        self.imgAA = result
+        self._imgAA = result
         return self
 
     def move(self, dx, dy):
-        self.img_xpos += dx
-        self.img_ypos += dy
+        self._img_xpos += dx
+        self._img_ypos += dy
         return self
         
-        resultX = []
-        result = []
-        
-        if dx > 0 & dx <= self.img_cols: # move right
-            for r in range(self.img_rows):
-                new_row = [fill]*dx
-                for c in range(self.img_cols-dx):
-                    new_row.append(self.imgAA[r][c])
-                resultX.append(new_row)
-        elif dx < 0 & dx >= -self.img_cols: # move left
-            for r in range(self.img_rows):
-                new_row = []
-                for c in range(-dx, self.img_cols):
-                    new_row.append(self.imgAA[r][c])
-                new_row += [fill] * (-dx)
-                resultX.append(new_row)
-        else: # dx == 0 or invalid
-            resultX = [row[:] for row in self.imgAA]
-
-        if dy > 0 & dy <= self.img_rows: # move down
-            for r in range(dy):
-                result.append([fill] * self.img_cols)
-            for r in range(self.img_rows - dy):
-                result.append(resultX[r])
-        elif dy < 0 & dy >= -self.img_rows: # move up
-            for r in range(-dy, self.img_rows):
-                result.append(resultX[r])
-            for r in range(-dy):
-                result.append([fill] * self.img_cols)
-        else: # dy == 0 or invalid
-            result = [row[:] for row in resultX]
-
-        self.imgAA = result
-        return self
-
     def imgPrint(self):
         str = ""
-        for row in self.imgAA:
+        for row in self._imgAA:
             for pixel in row:
                 str += pixel + " "
             str += "\n"
