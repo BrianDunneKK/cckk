d1098e40c74594b8f5b0ed54d632cbcb007a8796
BrianDunneKK <Brian.Dunne.KK@gmail.com>
2026-04-10 18:25:03 +0100

    Add simpler cckkCamera class

--- cckkCamera.py
+++ cckkCamera.py
@@ -7,11 +7,59 @@ except ImportError:
 import cckkCV
 from logzero import logger
 from os.path import join
+from time import sleep
 
 if not cckkCamera_ok:
     logger.warning("cckkCamera: picamzero module not found. Camera functionality will be disabled.")
 
 __version__ = "1.0.0"
+
+
+
+class cckkCamera():
+    _next_id = 1
+    _base_filename = "image"
+    _base_path = "./camera"
+    _camera = None
+
+    def available() -> bool:
+        return cckkCamera_ok
+
+    def _generate_filename() -> str:
+        filename = f"{cckkCamera._base_filename}_{cckkCamera._next_id:04d}.jpg"
+        cckkCamera._next_id += 1
+        return filename
+    
+    def __init__(self, auto_take=True):
+        if not cckkCamera_ok:
+            raise ImportError("cckkCamera: picamzero module not found. Cannot use cckkCamera.")
+
+        if cckkCamera._camera is None:
+            cckkCamera._camera = Camera()
+
+        if (auto_take):
+            self.take_photo()
+
+    @property
+    def filename(self) -> str:
+        return self._img_filename
+    
+    def take_photo(self) -> None:
+        self._img_filename = cckkCamera._generate_filename()
+        img_path = join(cckkCamera._base_path, self._img_filename)
+        cckkCamera._camera.take_photo(img_path)
+
+    def start_preview(self) -> None:
+        cckkCamera._camera.start_preview()
+
+    def stop_preview(self) -> None:
+        cckkCamera._camera.stop_preview()
+
+    def start_stop_preview(self, duration_secs: float) -> None:
+        self.start_preview()
+        sleep(duration_secs)
+        self.stop_preview()
+
 
 class cckkCameraORB(cckkCV.cckkCV2ORB):
     _next_id = 1
